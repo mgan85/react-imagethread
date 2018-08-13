0d0a907009aa99bf808ac53d43c778d9c05d2504
mgan85 <mgan@o2.pl>
2018-08-13 23:01:32 +0100

    create topbar, current number of views, current nober of posts

--- src/App/ImageThread/ImageThread.js
+++ src/App/ImageThread/ImageThread.js
@@ -3,16 +3,50 @@ import './ImageThread.css';
 import PostsList from './PostsList/PostsList';
 import ReplyBox from './ReplayBox/ReplyBox';
 import TopBar from './TopBar/TopBar';
+import {database} from "../Utils/firebase";
 
 class ImageThread extends Component {
-    state = {
-        posts: "a"
+    constructor() {
+        super();
+        this.state = {
+            views: "",
+            amountPosts: ""
+        }
+
+        this.incrementView();
+        this.getAmountPosts();
+    }
+
+    incrementView = () => {
+        let self = this;
+        database.ref('/AppInfo/views').once('value')
+            .then(function (snapshot) {
+                let views = parseInt(snapshot.val(), 10);
+                database.ref('/AppInfo/views')
+                    .set(views + 1);
+            });
+
+        database.ref('/AppInfo/views').on('value', function (snapshot) {
+            let views = parseInt(snapshot.val(), 10);
+            self.setState({views: views + 1});
+        });
+    }
+
+    getAmountPosts = () => {
+        let self = this;
+        database.ref('/AppInfo/currentPostID')
+        .on('value', function(snapshot) {
+            let postsID = parseInt(snapshot.val(), 10);
+            self.setState({amountPosts: postsID-1})
+        });
     }
 
     render() {
         return (
             <div>
-                <TopBar/>
+                <TopBar
+                    views={this.state.views}
+                    posts={this.state.amountPosts}/>
                 <ReplyBox/>
                 <PostsList/>
             </div>
--- src/App/ImageThread/TopBar/TopBar.js
+++ src/App/ImageThread/TopBar/TopBar.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './TopBar.css';
 
-const topBar = () => {
-    return <p>Top bar</p>
+const topBar = (props) => {
+    return (
+            <span>
+                <p>Posts: {props.posts}</p>
+                <input type="button" value="Export posts"/>
+                <p>Views: {props.views} </p>
+            </span>)
 }
 
 export default topBar;
